afbae761f4bf46a8d75bf8a27e7fb41d9bf65985
gl-school <gabriellinski@student.mitt.ca>
2024-05-17 11:32:03 -0500

    another attempted fix

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter as Router} from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
+     <Router basename="/gh-username-finder">
       <App />
     </Router>
   </React.StrictMode>
